1058058f9bfd778ea6cbe7270df42f3983a2036e
Arpit Mohankar <arpitmohankar28@gmail.com>
2024-03-14 18:53:24 +0530

    removed something

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const dotenv = require("dotenv");
 const connectDB = require("./Server/database/connection");
 
-dotenv.config({ path: "config.env" });
+//dotenv.config({ path: "config.env" });
 const PORT = process.env.PORT || 8080;
 
 connectDB();
